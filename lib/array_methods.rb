e13a179da86ac2ff08cd42ea92659e58fa11988e
Darin Webster <darinleenavy@gmail.com>
2019-06-17 20:08:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array([1, 2, 1, 3, 4, 3, 5, 4, 3, 2, 1])
+  array([5])
 end
 
 def find_min_value(array)
-  array[-1]
+  array[2]
 end
